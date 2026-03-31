a35862ba9fdb6152cb364f2ab2776f4ba845cbc3
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-03-31 09:11:18 -0300

    fix: user controller to permit normal users to list another users

--- src/main/java/com/ciro/backend/controller/UserController.java
+++ src/main/java/com/ciro/backend/controller/UserController.java
@@ -2,7 +2,6 @@ package com.ciro.backend.controller;
 
 import com.ciro.backend.dto.UserCreateDTO;
 import com.ciro.backend.dto.UserResponseDTO;
-import com.ciro.backend.entity.User;
 import com.ciro.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,30 +13,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserCreateDTO userDTO) {
         UserResponseDTO newUser = userService.createUser(userDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
-        return ResponseEntity.noContent().build(); // 204 No Content
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{id}/role")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<UserResponseDTO> toggleUserRole(@PathVariable Long id) {
         UserResponseDTO updatedUser = userService.toggleUserRole(id);
         return ResponseEntity.ok(updatedUser);
