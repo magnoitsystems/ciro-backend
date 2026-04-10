cdc7876815c02fbc9a3f10501bfd7af32c7278a1
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-04-10 09:38:28 -0300

    fix: details in controller from budgets

--- src/main/java/com/ciro/backend/controller/BudgetController.java
+++ src/main/java/com/ciro/backend/controller/BudgetController.java
@@ -2,41 +2,37 @@ package com.ciro.backend.controller;
 
 import com.ciro.backend.dto.BudgetCreateDTO;
 import com.ciro.backend.dto.BudgetResponseDTO;
-import com.ciro.backend.entity.Budget;
 import com.ciro.backend.service.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("api/v1/budgets")
+@RestController
+@RequestMapping("/api/v1/budgets")
 public class BudgetController {
+
     @Autowired
     private BudgetService budgetService;
 
-    @GetMapping()
-    public ResponseEntity<List<BudgetResponseDTO>> findAll(){
-        List<BudgetResponseDTO> budget = budgetService.findAll();
-
-        return new ResponseEntity<>(budget, HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<List<BudgetResponseDTO>> findAll() {
+        List<BudgetResponseDTO> budgets = budgetService.findAll();
+        return new ResponseEntity<>(budgets, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<BudgetResponseDTO> findById(@PathVariable Long id) {
         BudgetResponseDTO budgetDTO = budgetService.findById(id);
-
         return new ResponseEntity<>(budgetDTO, HttpStatus.OK);
     }
 
     @GetMapping("/patient/{id}")
-    public ResponseEntity<List<BudgetResponseDTO>> findByPatientId(@PathVariable Long id){
-        List<BudgetResponseDTO> budget = budgetService.findByPatientId(id);
-
-        return new ResponseEntity<>(budget, HttpStatus.OK);
+    public ResponseEntity<List<BudgetResponseDTO>> findByPatientId(@PathVariable Long id) {
+        List<BudgetResponseDTO> budgets = budgetService.findByPatientId(id);
+        return new ResponseEntity<>(budgets, HttpStatus.OK);
     }
 
     @PostMapping(consumes = { "multipart/form-data" })
@@ -46,15 +42,14 @@ public class BudgetController {
     }
 
     @PutMapping(value = "/{id}", consumes = { "multipart/form-data" })
-    public ResponseEntity<BudgetResponseDTO> updateBudget(@ModelAttribute BudgetCreateDTO budgetDTO, @PathVariable Long id){
+    public ResponseEntity<BudgetResponseDTO> updateBudget(@ModelAttribute BudgetCreateDTO budgetDTO, @PathVariable Long id) {
         BudgetResponseDTO response = budgetService.update(id, budgetDTO);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<BudgetResponseDTO> deleteBudget(@PathVariable Long id){
+    public ResponseEntity<BudgetResponseDTO> deleteBudget(@PathVariable Long id) {
         BudgetResponseDTO budgetDTO = budgetService.findById(id);
-
         budgetService.deleteById(id);
         return new ResponseEntity<>(budgetDTO, HttpStatus.OK);
     }
