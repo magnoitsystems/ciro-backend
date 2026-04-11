24617a78aa1a1f6ddce5c7c3cdbc959d2cbc2153
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-04-11 15:04:15 -0300

    fix: free task endpoints to test withouth login

--- src/main/java/com/ciro/backend/config/SecurityConfig.java
+++ src/main/java/com/ciro/backend/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
-
+                        .requestMatchers("/api/v1/tasks/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
