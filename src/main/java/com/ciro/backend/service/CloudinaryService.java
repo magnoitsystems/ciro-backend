e0f6ab8ad753c29c80728adf976160eb510df8a3
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-05-02 21:50:32 -0300

    fix: cloudinary service again

--- src/main/java/com/ciro/backend/service/CloudinaryService.java
+++ src/main/java/com/ciro/backend/service/CloudinaryService.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class CloudinaryService {
@@ -17,12 +18,31 @@ public class CloudinaryService {
 
     public String uploadFile(MultipartFile file) throws IOException {
         String resourceType = "auto";
+        String originalFilename = file.getOriginalFilename();
 
-        if (file.getContentType() != null && file.getContentType().equals("application/pdf")) {
-            resourceType = "raw";
+        String publicId = UUID.randomUUID().toString();
+
+        if (originalFilename != null && originalFilename.contains(".")) {
+            String baseName = originalFilename.substring(0, originalFilename.lastIndexOf("."));
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+            baseName = baseName.replaceAll("[^a-zA-Z0-9-_]", "_");
+
+            String shortUuid = UUID.randomUUID().toString().substring(0, 8);
+
+            publicId = baseName + "_" + shortUuid;
+            if (extension.equalsIgnoreCase(".pdf")) {
+                resourceType = "raw";
+                publicId += extension;
+            }
         }
 
-        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("resource_type", resourceType));
+        Map params = ObjectUtils.asMap(
+                "resource_type", resourceType,
+                "public_id", publicId
+        );
+
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), params);
 
         return uploadResult.get("secure_url").toString();
     }
